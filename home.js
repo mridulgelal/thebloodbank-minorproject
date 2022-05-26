c05e6a509d7fa0195ebd079730db8214907d0599
mridulgelal <mridul.gelal@gmail.com>
2022-05-26 22:39:00 +0545

    added login and signup and landing page

--- home.js
+++ home.js
@@ -1,7 +1,16 @@
-
+//------------------ user donation history drop down toggle--------------------------
 $(function () {
     $('.donor-card').click(function () {
         $(this).find('.user-history').slideToggle('show');
     });
 });
+
+
+
+// --------------------region and blood group selector form ------------------------
+
+
+
+
+
  
