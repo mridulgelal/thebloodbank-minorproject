67ef95d0cffba9e4af3442d8e25c72418e0f32a3
mridulgelal <mridul.gelal@gmail.com>
2022-08-03 18:56:47 +0545

    almost final ui except qr code

--- jsfiles/index.js
+++ jsfiles/index.js
@@ -1,57 +1,49 @@
 //------------------ user donation history drop down toggle--------------------------
 $(function () {
-    $('.donor-card').click(function () {
-        $(this).find('.user-history').slideToggle('show');
-    });
+  $(".donor-card").click(function () {
+    $(this).find(".user-history").slideToggle("show");
+  });
 });
-
-
 
 // --------------------region and blood group selector form ------------------------
 
 var subjectObject = {
-    "Bagmati": {
-        "Kathmandu": ["Links", "Images", "Tables", "Lists"],
-        "Bhaktapur": ["Borders", "Margins", "Backgrounds", "Float"],
-        "Lalitpur": ["Variables", "Operators", "Functions", "Conditions"]
-    },
-    "Gandaki": {
-        "Kaski": ["pokhara", "macchapuchre", "annapurna"],
-        "Parbat": ["modi", "bihadi", "kushma"]
-    },
-    "Lumbini": {
-        "PHP": ["Variables", "Strings", "Arrays"],
-        "SQL": ["SELECT", "UPDATE", "DELETE"]
-    }
-}
+  Bagmati: {
+    Kathmandu: ["kathmandu", "tarkeswor", "chandragiri", "budanilkantha"],
+    Bhaktapur: ["changunarayan", "madhyapur", "suryebinayek"],
+    Lalitpur: ["Godawari", "lalitpur", "mahalaxmi", "konjyosom"],
+  },
+  Gandaki: {
+    Kaski: ["pokhara", "macchapuchre", "annapurna"],
+    Parbat: ["modi", "bihadi", "kushma"],
+  },
+};
 window.onload = function () {
-    var subjectSel = document.getElementById("subject");
-    var topicSel = document.getElementById("topic");
-    var chapterSel = document.getElementById("chapter");
-   
-    for (var x in subjectObject) {
-        subjectSel.options[subjectSel.options.length] = new Option(x, x);
+  var subjectSel = document.getElementById("subject");
+  var topicSel = document.getElementById("topic");
+  var chapterSel = document.getElementById("chapter");
+
+  for (var x in subjectObject) {
+    subjectSel.options[subjectSel.options.length] = new Option(x, x);
+  }
+  subjectSel.onchange = function () {
+    //empty Chapters- and Topics- dropdowns
+    chapterSel.length = 1;
+    topicSel.length = 1;
+    //display correct values
+    for (var y in subjectObject[this.value]) {
+      topicSel.options[topicSel.options.length] = new Option(y, y);
     }
-    subjectSel.onchange = function () {
-        //empty Chapters- and Topics- dropdowns
-        chapterSel.length = 1;
-        topicSel.length = 1;
-        //display correct values
-        for (var y in subjectObject[this.value]) {
-            topicSel.options[topicSel.options.length] = new Option(y, y);
-        }
+  };
+  topicSel.onchange = function () {
+    //empty Chapters dropdown
+    chapterSel.length = 1;
+    //display correct values
+    var z = subjectObject[subjectSel.value][this.value];
+    for (var i = 0; i < z.length; i++) {
+      chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
     }
-    topicSel.onchange = function () {
-        //empty Chapters dropdown
-        chapterSel.length = 1;
-        //display correct values
-        var z = subjectObject[subjectSel.value][this.value];
-        for (var i = 0; i < z.length; i++) {
-            chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-        }
-    }
-}
+  };
+};
 
 // -------------------- -----------------------------------------------------------------
-
- 
